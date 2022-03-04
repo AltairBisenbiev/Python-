e77dd67174f8245b913070e5f3f90cb4d4b9a12d
AltairBisenbiev <99259963+AltairBisenbiev@users.noreply.github.com>
2022-03-04 19:50:02 +0600

    Delete Lab3 directory

--- Lab3/Functions1 1.py
+++
@@ -1,5 +0,0 @@
-def ountogram(grams):
-    ounces = 28.3495231 * grams
-    return(ounces)
-a = int(input())
-print(ountogram(a))
--- Lab3/Functions1 2.py
+++
@@ -1,5 +0,0 @@
-def temp(F):
-    C = (5 / 9) * (F - 32)
-    return(C)
-a = int(input())
-print(temp(a))
